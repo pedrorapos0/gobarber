c61d56d26fbc531435c4ab8e31b79587aa99934d
Pedro Raposo <pedroraposoneto@gmail.com>
2021-06-05 19:16:38 -0300

    Commit (Ajust Class to class duplicada)

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { classToClass, classToClass } from 'class-transformer';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
